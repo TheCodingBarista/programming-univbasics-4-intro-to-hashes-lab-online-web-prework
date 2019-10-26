147120f78425f49e7d4e520d51a02ba50917eccc
Melissa Holt <melissa.holt21@gmail.com>
2019-10-26 20:51:22 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -33,6 +33,6 @@ def read_from_hash(hash, key)
 end
 
 def update_counting_hash(hash, key)
-  hashes_on_hashes = Hash.new()
-  hash[:flying_keys] += 1
+  hashes_on_hashes = Hash.new(key)
+  hash[key] += 1
 end
