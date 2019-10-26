b9e41bb87f39a8b32a1265f93e9ba5ab90696f0e
Melissa Holt <melissa.holt21@gmail.com>
2019-10-26 21:26:58 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -34,7 +34,7 @@ end
 
 def update_counting_hash(hash, key)
   hashes_on_hashes = Hash.new(1)
-  #hashes_on_hashes["favorite number"] = 21
+  hashes_on_hashes["favorite number"] = 21
   hashes_on_hashes[key]
 end
 
